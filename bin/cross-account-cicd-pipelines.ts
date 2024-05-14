b5182ceee8dc2178782592a329036e0437f70968
juguedev <juguer.anra@gmail.com>
2024-05-13 23:11:09 -0500

    First deployable stacks, config/parameters.ts added but also ignored through gitignore

--- bin/cross-account-cicd-pipelines.ts
+++ bin/cross-account-cicd-pipelines.ts
@@ -2,18 +2,25 @@
 import * as cdk from 'aws-cdk-lib';
 import { PipelineStack } from '../lib/pipeline-stack';
 import { PipelineUtilsStack } from '../lib/pipeline-utils-stack';
-const env_target  = { account: '', region: '' };
-const env_devops = { account: '', region: '' };
+import {
+  TARGET_ACCOUNT,
+  TARGET_REGION,
+  DEVOPS_ACCOUNT,
+  DEVOPS_REGION
+} from '../config/parameters_dev';
+
+const env_target = { account: TARGET_ACCOUNT, region: TARGET_REGION };
+const env_devops = { account: DEVOPS_ACCOUNT, region: DEVOPS_REGION };
 
 const app = new cdk.App();
 
 const pipelineStack = new PipelineStack(app, "pipelineStack", {
-    env: env_devops,
-    env_name: "dev",
-    prefix: "testing"
+  env: env_devops,
+  env_name: "dev",
+  prefix: "testing"
 });
 
-const pipelineUtilsStack = new PipelineUtilsStack(app, "MyWestCdkStack", {
+const pipelineUtilsStack = new PipelineUtilsStack(app, "pipelineUtilsStack", {
   env: env_devops,
   env_name: "dev",
   prefix: "testing"
